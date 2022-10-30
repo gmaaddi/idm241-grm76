2e42b73cb4893392015274fb8298b62bd7cd59ab
gmaaddi <grm76@drexel.edu>
2022-10-30 16:47:15 -0400

    :bulb: Comments in progress for cleaner code

--- beta/build/main.js
+++ beta/build/main.js
@@ -15,6 +15,73 @@ let btnB = document.getElementById('btnB');
 let btnC = document.getElementById('btnC');
 let btnD = document.getElementById('btnD');
 
+// testmeclicking = function(imNum){
+
+//     box1.style.opacity = 0;
+//     box2.style.opacity = 0;
+//     box3.style.opacity = 0;
+//     box4.style.opacity = 0;
+
+
+//     btnA.src = 'card_images/dot.png';
+//     btnB.src = 'card_images/dot.png';
+//     btnC.src = 'card_images/dot.png';
+//     btnD.src = 'card_images/dot.png';
+
+//     switch(imNum){
+//         case 1:
+//             box1.style.opacity = 1;
+//             btnA.src = 'card_images/pawprint.png';
+//             img.src = 'card_images/pet-photo_1.png';
+//             alert("IN CASE 1");
+//             break;
+
+//         case 2:
+
+//             box2.style.opacity = 1;
+//             btnB.src = 'card_images/pawprint.png';
+//             img.src = 'card_images/pet-photo_2.png';
+//             alert("IN CASE 2");
+//             break;
+
+//         case 3:
+//             box3.style.opacity = 1;
+//             btnC.src = 'card_images/pawprint.png';
+//             img.src = 'card_images/pet-photo_3.png';
+//             alert("IN CASE 3");
+//             break;
+
+//         case 4:
+//             box2.style.opacity = 1;
+//             btnD.src = 'card_images/pawprint.png';
+//             img.src = 'card_images/pet-photo_4.png';
+//             alert("IN CASE 4");
+//             break;
+
+//         default:
+//             box1.style.opacity = 1;
+//             btnA.src = 'card_images/pawprint.png';
+//             img.src = 'card_images/pet-photo_1.png';
+//             break;
+//     }
+// }
+
+// a1.onclick = function(){
+//     testmeclicking(1);
+// }
+
+// a2.onclick = function(){
+//     testmeclicking(2);
+// }
+
+// a3.onclick = function(){
+//     testmeclicking(3);
+// }
+
+// a4.onclick = function(){
+//     testmeclicking(4);
+// }
+
 a1.onclick = function () {
     box1.style.opacity = 1;
     box2.style.opacity = 0;
